f0f8d9edaf3e618f62af02523ffae3741b1b60d3
Baku <Baku@SEID>
2024-11-23 18:57:52 +0400

    Add project files.

---
+++ seidcabbarli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { 
+        int derece = 25;
+        int  derece2 = (derece * 9 / 5) + 32;
+        Console.WriteLine(derece2);
+
+        
+            
+        
+           
+        
+    }
+}
